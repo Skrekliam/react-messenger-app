88772c158208f560f92cc8ac8ecaf4b857d51473
Maksym Skreklia <typnickman@gmail.com>
2021-02-15 19:08:10 +0200

    Adding example of firebase config

---
+++ src/firebase.config copy.js
@@ -0,0 +1,11 @@
+//Find your settings here:  https://console.firebase.google.com/u/_/project/_/overview
+const firebaseConfig = {
+    apiKey: /*your api key*/"" ,
+    authDomain: /* your projectId */".firebaseapp.com",
+    databaseURL: /* your projectId*/  /*server url */".firebasedatabase.app",
+    projectId: /* your projectId */"",
+    storageBucket: /* your projectId */".appspot.com",
+    messagingSenderId: /* messagingSenderId */"",
+    appId: /*appId*/ ""
+  };
+  export default firebaseConfig;
